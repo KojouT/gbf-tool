d05d65de3183378add5a156c2646fcfe01f6b812
KojouT <kojou.sieru.love@gmail.com>
2026-07-08 20:53:23 +0900

    weapons.jsonから武器選択プルダウンを生成

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 fetch("data/weapons.json")
     .then(response => response.json())
     .then(data => {
+
         console.log(data);
+
+        const weaponList = document.getElementById("weapon-list");
+    
+        // プルダウンを作成
+        const select = document.createElement("select");
+
+        // 武器を追加
+        data.forEach(weapon => {
+
+            const option = document.createElement("option");
+
+            option.value = weapon.id;
+            option.textContent = weapon.name;
+
+            select.appendChild(option);
+
+        });
+
+        weaponList.appendChild(select);
+
     });
